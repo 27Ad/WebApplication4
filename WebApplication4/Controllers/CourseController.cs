9d888e9cc49377328e23246eb381562a3bbcbf69
27Ad <ajy27998@gmail.com>
2022-05-04 18:30:03 +0530

    Add project files.

---
+++ WebApplication4/Controllers/CourseController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class CourseController : Controller
+    {
+        public efCore1Context db;
+        public CourseController(efCore1Context _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            var t = db.Course;
+            return View(t);
+        }
+
+        public IActionResult Index2()
+        {
+            var t = db.Course;
+            return View(t);
+        }
+        public IActionResult Addcourse()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Addcourse(Course c)
+        {
+            db.Course.Add(c);
+            db.SaveChanges();
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var Course = db.Course.Find(id);
+            db.Course.Remove(Course);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WebApplication4/Models/efCore1Context.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebApplication4.Models
+{
+    public partial class efCore1Context : DbContext
+    {
+        public efCore1Context()
+        {
+        }
+
+        public efCore1Context(DbContextOptions<efCore1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Course { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-QB1BEM2\\SQLEXPRESS;Database=efCore1;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.CourseName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
